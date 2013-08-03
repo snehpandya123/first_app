60ef59bf05f0b67c756f575234aa42cfe3b696ea
sneh <sapandya92@yahoo.co.in>
2013-08-03 20:19:50 +0530

    usernew

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 FirstApp::Application.routes.draw do
-  match '/contact', :to => 'pages#content' 
+  get "users/new"
+
+  match '/contact', :to => 'pages#contact' 
   
   match '/about', :to => 'pages#about'
   
   match '/help', :to => 'pages#help'
   
   match '/home', :to => 'pages#home'
+
+  match '/signup', :to => 'users#new'
 
   get "pages/home"
 
